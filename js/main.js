b3313065b56219c18cb85d6542bb833bcc319954
kkoorrooll <artjoker.group@gmail.com>
2017-09-20 17:41:28 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,130 @@
+
+function main() {
+
+(function () {
+   'use strict';
+   
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+	
+    // Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    });
+
+	// Hide nav on click
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
+	
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '.breakfast',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+    
+    //    Modal
+    $('#modal').on('shown.bs.modal',
+        function () {
+        $('#myInput').focus()
+            })
+        $('#popup').modal();
+    
+    //Slider
+    $(document).ready(function(){
+        $('.slider').slick({
+            dots: true,
+            prevArrow: false,
+            nextArrow: false,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ],
+            autoplay: true,
+            autoplaySpeed: 5000
+        });
+    });
+    
+    //    Popup
+    $('.show_popup').click(function() {
+        var popup_id = $('#' + $(this).attr("rel"));
+        $(popup_id).css({'top': $(window).scrollTop() +150}).show();
+        $('.overlay_popup').show();
+    })
+    $('.overlay_popup').click(function() {
+        $('.overlay_popup, .popup').hide();
+    })
+	
+}());
+
+
+}
+main();
+
+
+
